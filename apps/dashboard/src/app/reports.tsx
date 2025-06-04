529c2d4ea455d7204179ee24dee4be9b49b6f072
John James <john.usa.james@gmail.com>
2025-06-04 21:58:42 +0100

    chore: add reports component with supabase integration

--- apps/dashboard/src/app/reports.tsx
+++ apps/dashboard/src/app/reports.tsx
@@ -1,3 +1,44 @@
+import {useEffect, useState} from 'react';
+
+import type {VitestReport} from '@crackedmetrics/types';
+
+import supabase from '../utils/supabase';
+
+interface Report {
+  id: string;
+  raw_json: VitestReport;
+  run_at: string;
+}
+
 export function Reports() {
-  return <div>Reports</div>;
+  const [reports, setReports] = useState<Report[] | null>(null);
+
+  async function readSupabase() {
+    const {data, error} = await supabase.from('test_runs').select('*');
+    if (error) {
+      console.error(error);
+    }
+    setReports(data);
+  }
+
+  useEffect(() => {
+    readSupabase();
+  }, []);
+
+  return (
+    <div>
+      <h1>Reports</h1>
+      <button className="btn btn-outline" onClick={readSupabase}>
+        Refresh
+      </button>
+      <div>
+        {reports?.map((report) => (
+          <div key={report.id}>
+            {report.raw_json.success ? 'Success' : 'Failed'} {new Date(report.run_at).toLocaleString()}
+            <hr />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
