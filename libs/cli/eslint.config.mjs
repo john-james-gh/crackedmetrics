f99890934f55dec8fb959b5dd0f4676045afaf13
John James <john.usa.james@gmail.com>
2025-06-07 13:28:48 +0100

    chore: simplify eslint configuration

--- libs/cli/eslint.config.mjs
+++ libs/cli/eslint.config.mjs
@@ -1,10 +1,3 @@
 import baseConfig from '../../eslint.config.mjs';
 
-export default [
-  ...baseConfig,
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    // Override or add rules here
-    rules: {},
-  },
-];
+export default [...baseConfig];
