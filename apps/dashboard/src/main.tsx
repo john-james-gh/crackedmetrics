f66d07e5e55003a443a4edc240b636da3bbadba1
John James <john.usa.james@gmail.com>
2025-06-11 23:10:10 +0100

    feat: add account and organization routes

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,22 +1,8 @@
 import * as ReactDOM from 'react-dom/client';
 import {BrowserRouter, Route, Routes} from 'react-router';
 
-import {ApiKeyIndex} from './app/api-keys';
-import {ApiKeyCreate} from './app/api-keys/create';
-import {ApiKeyView} from './app/api-keys/view';
 import {Index} from './app/index';
 import {Layout} from './app/layout';
-import {OrganizationIndex} from './app/organizations';
-import {OrganizationCreate} from './app/organizations/create';
-import {OrganizationLayout} from './app/organizations/layout';
-import {OrganizationView} from './app/organizations/view';
-import {ProjectIndex} from './app/projects';
-import {ProjectCreate} from './app/projects/create';
-import {ProjectLayout} from './app/projects/layout';
-import {ProjectView} from './app/projects/view';
-import {ReportCreate} from './app/reports/create';
-import {ReportIndex} from './app/reports/index';
-import {ReportView} from './app/reports/view';
 import './styles.css';
 
 const root = document.getElementById('root') as HTMLElement;
@@ -27,33 +13,24 @@ ReactDOM.createRoot(root).render(
       <Route path="/" element={<Layout />}>
         <Route index element={<Index />} />
 
-        <Route path="organizations">
-          <Route index element={<OrganizationIndex />} />
-          <Route path="create" element={<OrganizationCreate />} />
+        <Route path="account" element={<div>Account Layout</div>}>
+          <Route index element={<h1>Account Overview/List of Org and Create Org Button via Modal</h1>} />
+          <Route path="activity" element={<h1>Account Activity</h1>} />
+          <Route path="settings" element={<h1>Account Settings</h1>} />
+        </Route>
 
-          <Route path=":organizationId" element={<OrganizationLayout />}>
-            <Route index element={<OrganizationView />} />
+        <Route path=":organizationId" element={<div>Organization Layout</div>}>
+          <Route index element={<h1>Org Overview/List of Projects and Create Project Button via Modal</h1>} />
+          <Route path="settings" element={<h1>Organization Settings</h1>} />
+          <Route path="activity" element={<h1>Organization Activity</h1>} />
+          <Route path="members" element={<h1>Organization Members</h1>} />
+          <Route path="usage" element={<h1>Organization Usage</h1>} />
 
-            <Route path="projects">
-              <Route index element={<ProjectIndex />} />
-              <Route path="create" element={<ProjectCreate />} />
-
-              <Route path=":projectId" element={<ProjectLayout />}>
-                <Route index element={<ProjectView />} />
-
-                <Route path="reports">
-                  <Route index element={<ReportIndex />} />
-                  <Route path="create" element={<ReportCreate />} />
-                  <Route path=":reportId" element={<ReportView />} />
-                </Route>
-
-                <Route path="api-keys">
-                  <Route index element={<ApiKeyIndex />} />
-                  <Route path="create" element={<ApiKeyCreate />} />
-                  <Route path=":apiKeyId" element={<ApiKeyView />} />
-                </Route>
-              </Route>
-            </Route>
+          <Route path=":projectId" element={<div>Project Layout</div>}>
+            <Route index element={<h1>Project Overview/Summary of Reports</h1>} />
+            <Route path="settings" element={<h1>Project Settings</h1>} />
+            <Route path="reports" element={<h1>List Of Reports / Create Report Button via Modal</h1>} />
+            <Route path="api-keys" element={<h1>List Of API Keys/Create API Key Button via Modal</h1>} />
           </Route>
         </Route>
       </Route>
