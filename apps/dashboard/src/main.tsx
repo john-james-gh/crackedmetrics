3a855a6bd156d631a6c62637141029a58972ea9f
John James <john.usa.james@gmail.com>
2025-06-11 23:15:19 +0100

    feat: update modal descriptions in routes

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -14,13 +14,29 @@ ReactDOM.createRoot(root).render(
         <Route index element={<Index />} />
 
         <Route path="account" element={<div>Account Layout</div>}>
-          <Route index element={<h1>Account Overview/List of Org and Create Org Button via Modal</h1>} />
+          <Route
+            index
+            element={
+              <h1>
+                Account Overview/List of Org and Create Org Button via Modal (modal should attach path or
+                search params)
+              </h1>
+            }
+          />
           <Route path="activity" element={<h1>Account Activity</h1>} />
           <Route path="settings" element={<h1>Account Settings</h1>} />
         </Route>
 
         <Route path=":organizationId" element={<div>Organization Layout</div>}>
-          <Route index element={<h1>Org Overview/List of Projects and Create Project Button via Modal</h1>} />
+          <Route
+            index
+            element={
+              <h1>
+                Org Overview/List of Projects and Create Project Button via Modal (modal should attach path or
+                search params)
+              </h1>
+            }
+          />
           <Route path="settings" element={<h1>Organization Settings</h1>} />
           <Route path="activity" element={<h1>Organization Activity</h1>} />
           <Route path="members" element={<h1>Organization Members</h1>} />
@@ -29,7 +45,14 @@ ReactDOM.createRoot(root).render(
           <Route path=":projectId" element={<div>Project Layout</div>}>
             <Route index element={<h1>Project Overview/Summary of Reports</h1>} />
             <Route path="settings" element={<h1>Project Settings</h1>} />
-            <Route path="reports" element={<h1>List Of Reports / Create Report Button via Modal</h1>} />
+            <Route
+              path="reports"
+              element={
+                <h1>
+                  List Of Reports/Create Report Button via Modal (modal should attach path or search params)
+                </h1>
+              }
+            />
             <Route path="api-keys" element={<h1>List Of API Keys/Create API Key Button via Modal</h1>} />
           </Route>
         </Route>
