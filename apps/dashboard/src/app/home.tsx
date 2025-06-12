0ab273f984f4d40afbf680eb4056983026fa12b3
John James <john.usa.james@gmail.com>
2025-06-12 17:04:54 +0100

    feat: add new input component and styles

--- apps/dashboard/src/app/home.tsx
+++ apps/dashboard/src/app/home.tsx
@@ -1,10 +1,7 @@
-import {Ui} from '@crackedmetrics/ui';
-
 export function Home() {
   return (
     <section className="flex flex-col gap-y-2">
       <h1 className="text-2xl font-bold">Home Page</h1>
-      <Ui />
     </section>
   );
 }
