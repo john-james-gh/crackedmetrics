0b883fb945307c96bf91a4a31df458bbd43c4326
John James <john.usa.james@gmail.com>
2025-06-13 23:03:08 +0100

    fix: update heading text in example spec

--- apps/dashboard-e2e/src/example.spec.ts
+++ apps/dashboard-e2e/src/example.spec.ts
@@ -4,5 +4,5 @@ test('has title', async ({page}) => {
   await page.goto('/');
 
   // Expect h1 to contain a substring.
-  expect(await page.getByRole('link', {name: 'Home'}).innerText()).toContain('Home');
+  expect(await page.getByRole('heading', {level: 1}).innerText()).toContain('Home Page');
 });
