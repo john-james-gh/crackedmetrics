b0a29fb3369eb753eff2b7be3582f3f4e4bd17a3
John James <57100733+john-james-gh@users.noreply.github.com>
2025-06-17 15:24:34 +0100

    Update cli.ts

--- libs/cli/src/lib/cli.ts
+++ libs/cli/src/lib/cli.ts
@@ -5,9 +5,8 @@ import {spawn} from 'child_process';
 import 'dotenv/config';
 import fs from 'fs/promises';
 
-const SUPABASE_URL = 'https://ttuqputnayjfqwwdzorw.supabase.co';
-const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0dXFwdXRuYXlqZnF3d2R6b3J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5ODYwOTYsImV4cCI6MjA2NDU2MjA5Nn0.fAiOkJf5a6qA5UXWLsB18kBxlYkxMTtGAuh9tVksefE';
+const SUPABASE_URL = ''
+const SUPABASE_ANON_KEY = ''
 
 const API_KEY = process.env.CRACKED_API_KEY; // Project API Key
 
